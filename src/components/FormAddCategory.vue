c9dd8cf51f7a1b992ce21cefa04d3de43880b166
simonkrogh10 <53627659+simonkrogh10@users.noreply.github.com>
2020-12-03 13:06:26 +0100

    Created AddCategory component
    the select items are missing icon+ color.

---
+++ src/components/FormAddCategory.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-row>
+    <v-select v-model="item" :items="categories" label="Outlined style" outlined item-text="name">
+          <template  v-slot="{ item }" >
+              <v-list-item>
+                    <v-list-item-content>
+                        <v-list-item-icon><v-icon :color="item.color">mdi-circle</v-icon></v-list-item-icon>
+                         <v-list-item-title> {{ item.name }} </v-list-item-title>
+                    </v-list-item-content>
+              </v-list-item>
+              
+          </template>
+    </v-select>
+  </v-row>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component({})
+export default class FormAddCategory extends Vue {
+  @Prop({
+     required: true, 
+  })
+  categories!: {name: string; color: string}[];
+  
+  item = 'Arbejde';
+    
+}
+</script>
