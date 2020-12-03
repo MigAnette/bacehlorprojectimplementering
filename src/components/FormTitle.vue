a2b6061e1672490cefffd53dcc54848e4771951f
Mikkel Kjær Andersen <mikkelka@gmail.com>
2020-12-03 11:22:48 +0100

    Timepicker component

---
+++ src/components/FormTitle.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-row>
+    <v-text-field v-model="title" label="Regular" @input="changeTitle">
+    </v-text-field>
+  </v-row>
+</template>
+
+<script lang='ts'>
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component({})
+export default class FormTitle extends Vue {
+  @Prop({})
+  addTitle!: string;
+
+  title = this.addTitle;
+  public changeTitle() {
+    this.$emit("changeTitle", this.title);
+  }
+}
+</script>
