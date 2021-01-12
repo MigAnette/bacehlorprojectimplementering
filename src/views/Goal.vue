1ccbffe9062f1be311f8cbcac87fd615a36027c6
Anette Sommer <anettesommer@live.dk>
2021-01-12 13:22:17 +0100

    Icon has been added to menu, margin hass been added to OpenTaskDialog, wherthe icon button icon checkmark is and the text. A new page called Goal has been added. The firebase init page has been added to the gitignore, hopefully it works.

---
+++ src/views/Goal.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>Mål</div>
+</template>
+
+<script lang='ts'>
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component({
+})
+
+export default class Goal extends Vue {
+}
+</script>
